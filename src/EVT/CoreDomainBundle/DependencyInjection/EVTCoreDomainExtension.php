58f0bb9570121f1db5d61d6615eaffd7089c1b23
Eduardo Gulias Davis <eduardo.gulias@bodaclick.com>
2014-01-15 10:32:34 +0100

    [#EVT-22] - Services config via yaml

--- src/EVT/CoreDomainBundle/DependencyInjection/EVTCoreDomainExtension.php
+++ src/EVT/CoreDomainBundle/DependencyInjection/EVTCoreDomainExtension.php
@@ -7,11 +7,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class EVTCoreDomainExtension extends Extension
 {
     /**
@@ -22,7 +17,7 @@ class EVTCoreDomainExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
