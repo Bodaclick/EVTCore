13129217fbb50cb2363d03a2e1fcea5bb89c68d7
Marco Ferrari <marco.ferrari@bodaclick.com>
2014-01-16 17:31:01 +0100

    [#EVT-30] - Provider manager management adde

--- src/EVT/CoreDomain/Tests/Provider/ProviderTest.php
+++ src/EVT/CoreDomain/Tests/Provider/ProviderTest.php
@@ -4,6 +4,7 @@ namespace EVT\CoreDomain\Tests\Provider;
 use EVT\CoreDomain\Email;
 use EVT\CoreDomain\EmailCollection;
 use EVT\CoreDomain\User\Manager;
+use EVT\CoreDomain\User\PersonalInformation;
 use EVT\CoreDomain\Provider\ProviderId;
 use EVT\CoreDomain\Provider\Provider;
 
@@ -27,13 +28,13 @@ class ProviderTest extends \PHPUnit_Framework_TestCase
     {
         $testName = "TestName";
         $provider = new Provider(new ProviderId(''), $testName, new EmailCollection(new Email('valid@email.com')));
-        $provider->addManager(new Manager('Name', 'valid@email.com'));
+        $provider->addManager(new Manager('valid@email.com', new PersonalInformation()));
 
         $managers = $provider->getManagers();
         $this->assertCount(1, $managers);
 
 
-        $provider->addManager(new Manager('Name', 'valid@email.com'));
+        $provider->addManager(new Manager('valid@email.com', new PersonalInformation()));
 
         $managers = $provider->getManagers();
         $this->assertCount(1, $managers);
