f694fe5bb1d16e344c616d423ddc525a4759b126
Mariosten <mcazorla@bodaclick.com>
2014-01-16 18:22:00 +0100

    test iniciales del proyecto y clase

---
+++ src/EVT/CoreDomain/Lead/EventType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace EVT\CoreDomain\Lead;
+
+/**
+ * EventType
+ *
+ * @author    Mario Cazorla  <mcazorla@bodaclick.com>
+ * @copyright 2014 Bodaclick S.A
+ */
+class EventType
+{
+
+    private $type;
+
+    public function __construct($type)
+    {
+
+        $this->type = $type;
+    }
+    public function getType()
+    {
+        return $this->type;
+    }
+}
---
+++ src/EVT/CoreDomain/Lead/Location.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace EVT\CoreDomain\Lead;
+
+/**
+ * EventType
+ *
+ * @author    Mario Cazorla  <mcazorla@bodaclick.com>
+ * @copyright 2014 Bodaclick S.A
+ */
+class Location
+{
+
+    private $lat;
+    private $long;
+    private $adminLevel1;
+    private $adminLevel2;
+    private $country;
+
+    public function __construct($lat,$long, $adminLevel1, $adminLevel2, $country)
+    {
+
+        $this->lat = $lat;
+        $this->long = $long;
+        $this->adminLevel1=$adminLevel1;
+        $this->adminLevel2=$adminLevel2;
+        $this->country=$country;
+
+    }
+    public function getLatLong()
+    {
+
+        $latLong= array('lat'=>$this->lat,'long'=>$this->long);
+        return $latLong;
+
+
+    }
+
+    public function getAdminLevel1()
+    {
+        return $this->adminLevel1;
+
+    }
+    public function getAdminLevel2()
+    {
+        return $this->adminLevel2;
+
+    }
+    public function getCountry()
+    {
+        return $this->country;
+
+    }
+
+}
