275235def49c6b2d034032b693adfd83e6de94fe
Mariosten <mcazorla@bodaclick.com>
2014-01-17 14:37:21 +0100

    [#EVT-26] - Showing test to admin

--- src/EVT/ApiBundle/Tests/Controller/ForceHttpsTest.php
+++ src/EVT/ApiBundle/Tests/Controller/ForceHttpsTest.php
@@ -1,28 +1,22 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: mcazorla
- * Date: 17/01/14
- * Time: 13:10
- * To change this template use File | Settings | File Templates.
- */
 
 namespace EVTCore\ApiBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
 
 class RoutingControllerTest extends WebTestCase
 {
     public function testHttps()
     {
         $client = static::createClient(array(),array('HTTPS' => true));
-        $crawler = $client->request('GET', '/EVT/ApiBundle/Controller');
-        $this->assertGreaterThan(0, $crawler->filter('que pongo aqui?')->count());
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse($crawler)->getStatusCode());
     }
-    public function testHttp()
+    public function testHttpNotAllowed()
     {
         $client = static::createClient(array(),array('HTTPS' => false));
-        $crawler = $client->request('GET', '/EVT/ApiBundle/Controller');
-        $this->assertEquals(0, $crawler->filter('que pongo auqi?')->count());
+        $crawler = $client->request('GET', '/');
+        $this->assertNotEquals(200, $client->getResponse($crawler)->getStatusCode());
     }
 }
