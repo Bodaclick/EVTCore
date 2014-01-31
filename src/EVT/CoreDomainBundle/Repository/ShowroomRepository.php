15d63a84355710ae8554509c41d6cbcaf5b5f76f
Eduardo Gulias Davis <eduardo.gulias@bodaclick.com>
2014-01-31 13:37:33 +0100

    [#EVT-9] - Showroom repo

--- src/EVT/CoreDomainBundle/Repository/ShowroomRepository.php
+++ src/EVT/CoreDomainBundle/Repository/ShowroomRepository.php
@@ -4,17 +4,24 @@ namespace EVT\CoreDomainBundle\Repository;
 
 use EVT\CoreDomain\Provider\ShowroomRepositoryInterface as DomainRepository;
 use Doctrine\ORM\EntityRepository;
+use EVT\CoreDomainBundle\Mapping\ShowroomMapping;
 
 /**
- * UserRepository
- *
+ * Class ShowroomRepository
  * @author Eduardo Gulias Davis <eduardo.gulias@bodaclick.com>
- * @copyright Bodaclick S.A
+ * @copyright 2014 Bodaclick
  */
 class ShowroomRepository extends EntityRepository implements DomainRepository
 {
+
+    private $mapping;
+
     public function save($showroom)
     {
+        $entity = $this->mapping->mapDomainToEntity($showroom);
+        $this->_em->persist($entity);
+        $this->_em->flush();
+        $this->setId($entity->getId(), $showroom);
     }
 
     public function delete($showroom)
@@ -27,5 +34,18 @@ class ShowroomRepository extends EntityRepository implements DomainRepository
 
     public function findAll()
     {
+    }
+
+    public function setMapper(ShowroomMapping $mapping)
+    {
+        $this->mapping = $mapping;
+    }
+
+    private function setId($id, $showroom)
+    {
+        $rflShowroom = new \ReflectionClass($showroom);
+        $rflId = $rflShowroom->getProperty('id');
+        $rflId->setAccessible(true);
+        $rflId->setValue($showroom, $id);
     }
 }
