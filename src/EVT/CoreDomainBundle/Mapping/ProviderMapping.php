7139f120808461a2d92a44fae8d9e4d17dd2674c
Marco Ferrari <marco.ferrari@bodaclick.com>
2014-01-31 10:27:28 +0100

    [#EVT-41] - Mapper created

---
+++ src/EVT/CoreDomainBundle/Mapping/ProviderMapping.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace EVT\CoreDomainBundle\Mapping;
+
+use Doctrine\ORM\EntityManager;
+use EVT\CoreDomain\Provider\Provider;
+
+/**
+ * ProviderMapping
+ *
+ * @author    Marco Ferrari <marco.ferrari@bodaclick.com>
+ * @copyright 2014 Bodaclick S.A
+ */
+class ProviderMapping
+{
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function mapDomainToEntity(Provider $provider)
+    {
+        $eProvider = new \EVT\CoreDomainBundle\Entity\Provider();
+
+        if (null !== $provider->getId()) {
+            $rflProvider = new \ReflectionClass($eProvider);
+            $rflId = $rflProvider->getProperty('id');
+            $rflId->setAccessible(true);
+            $rflId->setValue($eProvider, $provider->getId());
+        }
+
+        $eProvider->setName($provider->getName());
+        $eProvider->setSlug($provider->getSlug());
+        $eProvider->setPhone($provider->getPhone());
+
+        $managers = $provider->getManagers()->getIterator();
+
+        foreach ($managers as $manager) {
+            $managerProxy = $this->em->getReference('EVT\CoreDomainBundle\Entity\GenericUser', $manager->getId());
+            $eProvider->addGenericUser($managerProxy);
+        }
+
+        return $eProvider;
+    }
+}
