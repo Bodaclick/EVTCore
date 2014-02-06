16e2fc6213f29a79caf0fb9cf8407c25cdcf5adc
Quique Torras <etorras@gmail.com>
2014-02-06 16:25:10 +0100

    [#EVT-9] - Serializer for showroom

--- src/EVT/CoreDomainBundle/Tests/Repository/ShowroomRepositoryTest.php
+++ src/EVT/CoreDomainBundle/Tests/Repository/ShowroomRepositoryTest.php
@@ -14,30 +14,54 @@ use EVT\CoreDomain\Provider\Showroom as DomainShowroom;
  */
 class ShowroomRepositoryTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSave()
+    protected $provider;
+    protected $vertical;
+    protected $showroom;
+    protected $asyncDispatcher;
+    protected $srMapper;
+    protected $emMock;
+    protected $metadata;
+
+    public function setUp()
     {
-        $provider = $this->getMockBuilder('EVT\CoreDomain\Provider\Provider')->disableOriginalConstructor()->getMock();
-        $vertical = $this->getMockBuilder('EVT\CoreDomain\Provider\Vertical')->disableOriginalConstructor()->getMock();
-        $domainMock = new DomainShowroom($provider, $vertical, 0);
-        $showroom = new Showroom();
+        $this->provider = $this->getMockBuilder('EVT\CoreDomain\Provider\Provider')->disableOriginalConstructor()->getMock();
+        $this->vertical = $this->getMockBuilder('EVT\CoreDomain\Provider\Vertical')->disableOriginalConstructor()->getMock();
+        $this->showroom = new DomainShowroom($this->provider, $this->vertical, 0);
 
-        $srMapper = $this->getMockBuilder('EVT\CoreDomainBundle\Mapping\ShowroomMapping')
+        $this->srMapper = $this->getMockBuilder('EVT\CoreDomainBundle\Mapping\ShowroomMapping')
             ->disableOriginalConstructor()->getMock();
-        $srMapper->expects($this->once())->method('mapDomainToEntity')->will($this->returnValue($showroom));
-        $emMock = $this->getMockBuilder('Doctrine\ORM\EntityManager')->disableOriginalConstructor()->getMock();
-        $emMock->expects($this->once())->method('persist')->will($this->returnSelf());
-        $emMock->expects($this->once())->method('flush')->will($this->returnSelf());
-        $metadata = $this->getMockBuilder('Doctrine\ORM\Mapping\ClassMetadata')
-            ->disableOriginalConstructor()->getMock();
+        $this->srMapper->expects($this->once())->method('mapDomainToEntity')->will($this->returnValue(new Showroom()));
 
-        $asyncDispatcher = $this->getMockBuilder('BDK\AsyncDispatcherBundle\Model\EventDispatcher\AsyncEventDispatcher')
+        $this->emMock = $this->getMockBuilder('Doctrine\ORM\EntityManager')->disableOriginalConstructor()->getMock();
+        $this->emMock->expects($this->once())->method('persist')->will($this->returnSelf());
+        $this->emMock->expects($this->once())->method('flush')->will($this->returnSelf());
+        $this->metadata = $this->getMockBuilder('Doctrine\ORM\Mapping\ClassMetadata')
             ->disableOriginalConstructor()->getMock();
 
-        $asyncDispatcher->expects($this->once())->method('dispatch')->will($this->returnValue($this->returnSelf()));
+        $this->asyncDispatcher = $this->getMockBuilder('BDK\AsyncDispatcherBundle\Model\EventDispatcher\AsyncEventDispatcher')
+            ->disableOriginalConstructor()->getMock();
 
-        $repo = new ShowroomRepository($emMock, $metadata);
-        $repo->setMapper($srMapper);
-        $repo->setAsyncDispatcher($asyncDispatcher);
-        $repo->save($domainMock);
+        $this->asyncDispatcher->expects($this->once())->method('dispatch')->will($this->returnValue($this->returnSelf()));
+    }
+
+    public function testSaveCreate()
+    {
+        $repo = new ShowroomRepository($this->emMock, $this->metadata);
+        $repo->setMapper($this->srMapper);
+        $repo->setAsyncDispatcher($this->asyncDispatcher);
+        $repo->save($this->showroom);
+    }
+
+    public function testSaveUpdate()
+    {
+        $rflShowroom = new \ReflectionClass($this->showroom);
+        $rflId = $rflShowroom->getProperty('id');
+        $rflId->setAccessible(true);
+        $rflId->setValue($this->showroom, 1);
+
+        $repo = new ShowroomRepository($this->emMock, $this->metadata);
+        $repo->setMapper($this->srMapper);
+        $repo->setAsyncDispatcher($this->asyncDispatcher);
+        $repo->save($this->showroom);
     }
 }
